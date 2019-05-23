b5f48a0939786c51131deb53ba449871298c9baf
Laure-Marie <lauremarie.torre@gmail.com>
2019-05-23 17:30:52 +0200

    FIN EXAMEN

--- Base/js/script.js
+++ Base/js/script.js
@@ -1,13 +1,14 @@
 $.noConflict();
 $(document).ready(function(){
+
+	// jQUery pour les onglets date
+	$(":date").bind("onShow onHide", function()  {
+	$(this).parent().toggleClass("active");
+	});
 	
+// ISOTOPE
 
+$('.tri').on('click', function(){
+	$('.grid').isotope({getSortData : {number: 'span parseInt'}, sortBy : 'number', sortAscending: true});
+});
 
-
-
-
-
-
-
-
-}
